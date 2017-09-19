6a8c51d3d87f49ad55aef00873963609eae16482
Marcel Cruz <marcelcruz@live.com>
2017-09-19 14:23:36 +0100

    library list and connect added

---
+++ src/components/LibraryList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+  render() {
+    console.log(this.props);
+    return;
+  }
+}
+
+const mapStatetoProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStatetoProps)(LibraryList);
